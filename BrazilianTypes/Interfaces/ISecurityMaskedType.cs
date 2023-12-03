cc83ee98a16954600a363c4bfa1e8bd1cfb86f7a
Huam Benvenutti <huam.benvenutti@vortigo.com.br>
2023-12-03 15:33:33 -0300

    doc

--- BrazilianTypes/Interfaces/ISecurityMaskedType.cs
+++ BrazilianTypes/Interfaces/ISecurityMaskedType.cs
@@ -1,6 +1,10 @@
 namespace BrazilianTypes.Interfaces;
 
+/// <summary>
+/// Interface to add security mask to data annotations.
+/// </summary>
 public interface ISecurityMaskedType
 {
+    /// <summary>Gets the security mask.</summary>
     string SecurityMask { get; }
 }
